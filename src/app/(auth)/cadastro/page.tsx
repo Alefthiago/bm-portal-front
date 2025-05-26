c2040925933baeb7cb01a929dad541df792d0be2
Alef Thiago da Silva <alefsilvadn@gmail.com>
2025-05-25 22:55:09 -0300

    Primeiro commit.
    Criações iniciais e testes de elementos,

---
+++ src/app/(auth)/cadastro/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <section>
+        <h1 className="text-2xl font-bold">Cadastro</h1>
+        <p className="text-gray-600">Preencha os campos abaixo para se cadastrar.</p>
+        {/* Aqui você pode adicionar o formulário de cadastro */}
+        {/* Exemplo: <CadastroForm /> */}
+        </section>
+    );
+}
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import { LoginForm } from "@/components/auth/form-login";
+import { FaGoogle } from "react-icons/fa";
+import {
+  
+} from "lucide-react"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
+
+export default function Page() {
+  return (
+    <section className={`p-6 sm:w-[500px] w-full`}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Bm Portal</CardTitle>
+          <CardDescription>Realize o login</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button variant="destructive" className={`cursor-pointer`}><FaGoogle /> Entrar com Google</Button>
+        </CardFooter>
+      </Card>
+    </section >
+  );
+}
